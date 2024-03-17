07b108cb16d3d444ea909d133b51b415f3cc1fdf
macbook <harisgorcevic1000@gmail.com>
2024-03-17 22:25:09 +0100

    Work

--- vezbanje/app.js
+++ vezbanje/app.js
@@ -6268,4 +6268,14 @@
 
 // console.log(indexes);
 // ===================================================================================
-// ===================================================================================
+const array = [1, 2, 3, 4, 5, 6, 2, 2];
+const emptyArray = [];
+
+const findIndex = array.filter((data, index) => {
+  if (data === 2) {
+    emptyArray.push(index);
+  } else {
+    return null;
+  }
+});
+console.log(emptyArray);
